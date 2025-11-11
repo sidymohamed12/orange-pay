48df97ef799e603baaef8aeed938063a7fd66ae1
Sidy <mohamedsaizonou86@gmail.com>
2025-11-11 20:55:57 +0000

    implementation du service et d'une exception not found

--- src/main/java/orange/pay/orange_pay/services/impl/TransactionService.java
+++ src/main/java/orange/pay/orange_pay/services/impl/TransactionService.java
@@ -4,16 +4,21 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import orange.pay.orange_pay.repository.ICompteRepository;
 import orange.pay.orange_pay.repository.ITransactionRepository;
 import orange.pay.orange_pay.services.ITransactionService;
+import orange.pay.orange_pay.utils.exceptions.ResourceNotFound;
 import orange.pay.orange_pay.web.dto.response.HistoriqueTransactionResponse;
+import orange.pay.orange_pay.web.dto.response.TransactionOneResponse;
 import orange.pay.orange_pay.web.mappers.TransactionMapper;
 
 @Service
 @RequiredArgsConstructor
 public class TransactionService implements ITransactionService {
     private final ITransactionRepository transactionRepository;
+    private final ICompteRepository compteRepository;
     private final TransactionMapper transactionMapper;
 
     @Override
@@ -22,5 +27,22 @@ public class TransactionService implements ITransactionService {
                 .stream()
                 .map(transactionMapper::toHistoriqueTransactionResponse)
                 .toList();
+    }
+
+    @Override
+    public TransactionOneResponse getTransactionById(@NonNull Long id) {
+        return transactionRepository.findById(id)
+                .map(transac -> {
+                    TransactionOneResponse transaction = transactionMapper.toTransactionOneResponse(transac);
+                    transaction.setNomDestinataire(getNomCompteByNumero(transac.getNumeroDestinataire()));
+                    return transaction;
+                })
+                .orElseThrow(() -> new ResourceNotFound("Transaction not found with id " + id));
+    }
+
+    private String getNomCompteByNumero(@NonNull String numero) {
+        return compteRepository.findByNumero(numero)
+                .map(cpt -> cpt.getNom() + " " + cpt.getPrenom())
+                .orElseThrow(() -> new ResourceNotFound("Compte not found with numero " + numero));
     }
 }
