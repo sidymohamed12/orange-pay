f9bc0476bee4a8bf2c294783a352832188ca0ee6
Sidy <mohamedsaizonou86@gmail.com>
2025-11-11 16:11:09 +0000

    first commit

---
+++ src/main/java/orange/pay/orange_pay/OrangePayApplication.java
@@ -0,0 +1,13 @@
+package orange.pay.orange_pay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrangePayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrangePayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/orange/pay/orange_pay/OrangePayApplicationTests.java
@@ -0,0 +1,13 @@
+package orange.pay.orange_pay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrangePayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
